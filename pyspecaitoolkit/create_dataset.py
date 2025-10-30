4d2ec6f074250b1b9f2cee30c0ff3928b27366fd
Grogu Melo <melostrader@gmail.com>
2025-10-30 14:43:17 -0400

    feat(dataset): create_dataset.py vs 1.0.0

--- pyspecaitoolkit/create_dataset.py
+++ pyspecaitoolkit/create_dataset.py
@@ -370,7 +370,7 @@ def build_master_dataset(
 
     # 1. Defina sua "ordem ideal" de colunas.
     #    Use as constantes que importamos de protocols_config.py
-    ideal_column_order = [gi
+    ideal_column_order = [
         # IDs Principais
         UNIQUE_ID_COL_STD,
         FILENAME_COL_STD,
